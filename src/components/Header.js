d220845a4575e0477ec0701388ffc55d14f39283
Philippine Lorrain <philippinelorrain@gmail.com>
2022-06-28 16:49:39 -0400

    learning react

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+
+  const onClick = () => {
+    console.log('Click')
+  }
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='pink' text='Add' onClick={onClick}/>
+    </header>
+  )
+}
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// in h1 tag : style={{ color: 'red', backgroundColor: 'blue' }}
+// OR CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'blue'
+//   replace with headingStyle in single{} in h1 style
+// }
+export default Header
---
+++ src/components/Tasks.js
@@ -0,0 +1,28 @@
+const tasks = [
+{
+  id: 1,
+  text: 'Doctors Appointment',
+  day: 'Feb 5th at 2:30pm',
+  reminder: true,
+},
+{
+  id: 2,
+  text: 'Meeting at school',
+  day: 'Feb 6th at 1:30pm',
+  reminder: true,
+},
+{
+  id: 3,
+  text: 'Grocery store',
+  day: 'Feb 6th at 2:30pm',
+  reminder: false,
+}
+
+]
+const Tasks = () => {
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
