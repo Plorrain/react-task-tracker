81c0fc5198eabe62467c2c3dca108fb762bb2aeb
Philippine Lorrain <philippinelorrain@gmail.com>
2022-06-30 19:36:35 -0400

    learning react-app comes along nicely-all from the crash course

--- src/App.js
+++ src/App.js
@@ -27,14 +27,27 @@ const App = () => {
 
   // Delete task
   const deleteTask = (id) => {
-  console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !==
+    id))
+  }
+
+  // Toggle Reminder
+  const  toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ?
+    {...task, reminder: !task.reminder} : task
+      )
+    )
   }
 
   return (
-  <div className ='container'>
-      <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
-  </div>
+    <div className ='container'>
+        <Header />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder} />
+      ) : (
+      'No tasks To Show'
+      )}
+    </div>
   );
 }
 
